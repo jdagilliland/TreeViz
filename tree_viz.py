8efaa41e89e6a2c9df82bfae62e61cb1817e7fe2
Jason Daniel Augustine Gilliland <jdagilliland@gmail.com>
2014-03-18 08:52:08 -0400

    Start writing tree_viz
    The purpose of tree_viz is to visualize Newick trees with ete2.

---
+++ tree_viz.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+import csv
+
+import ete2
+
+def print_tree(fname, **kwopts):
+    """
+    Print a tree read from a `fname`.
+
+    Parameters
+    ----------
+    fname : str
+        The file name to use to print the tree from.
+
+    Returns
+    -------
+    None
+    """
+    tree = ete2.Tree(fname)
+    return None
+
+def color_nodes(tree, tabfile, column, dict_color=None):
+    """
+    Color nodes of `tree` according to data in `tabfile`.
+
+    Parameters
+    ----------
+    tree : ete2.tree.TreeNode 
+        The tree to color.
+    tabfile : str
+        The filename of the `tabfile` from which to draw data.
+    column : str
+        A string to indicate the column of the `tabfile` to use.
+    dict_color : dict, optional
+        A dictionary of colors to use based on the values in the
+        selected `column`.
+        If not provided, a default dictionary of appropriately spaced colors
+        will be constructed for all values found in `column`.
+
+    Returns
+    -------
+    tree : ete2.tree.TreeNode
+        The colored `tree`.
+
+    """
+    if dict_color == None:
+        dict_color = _get_color_dict()
+    with open(tabfile,'rb') as f:
+        reader = csv.DictReader(f,delimiter='\t')
+        lst_dict_entries = [row for row in reader]
+    for node in tree.traverse():
+        try:
+            dict_entry = get_node_entry(node.name, lst_dict_entries)
+        except ValueError:
+            continue
+        column_data = dict_entry.get(column)
+        color = dict_color.get(column_data,'none')
+        node.color = color
+        style = ete2.NodeStyle()
+        style['fgcolor'] = color
+        node.set_style(style)
+    return tree
+
+def _get_node_entry(nodename, lst_dict_entries):
+    for dict_entry in lst_dict_entries:
+        if dict_entry['SEQUENCE_ID'][-9:] == nodename:
+            return dict_entry
+        else: continue
+    raise ValueError('Nodename {name} not found'.format(name=nodename))
+
+def _get_color_dict(lst_col_data):
+    set_data = set(lst_col_data)
+    return dict((data, ete2.random_color()) for data in set_data)
+    
